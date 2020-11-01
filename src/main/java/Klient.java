b1a10e69880993ffc2fcd3921cfbf51194661e77
Alexey Sharaburin <alexey.sharaburin@gmail.com>
2020-11-01 12:41:12 +0100

    New KlientServer

--- src/main/java/Klient.java
+++ src/main/java/Klient.java
@@ -4,25 +4,50 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Klient {
     public static void main(String[] args) {
-        String host = "127.0.0.1";
-        int port = 8088;
+        Scanner scanner = new Scanner(System.in);
+        String host = "netology.homework";
+        int port = 7777;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.write("Alexey Sharaburin" + "\r\n");
-            out.flush();
 
-            String resp = in.readLine();
-            System.out.println(resp);
+            readIn(in);
+
+            String nameKlient = scanner.nextLine();
+            writeOut(out, nameKlient);
+
+            readIn(in);
+
+            String answerKlient = scanner.nextLine();
+
+            if ("yes".equals(answerKlient)) {
+                writeOut(out, answerKlient);
+            } else {
+                writeOut(out, answerKlient);
+            }
+
+            readIn(in);
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void writeOut(PrintWriter out, String string) {
+        out.write(string);
+        out.write("\r\n");
+        out.flush();
+    }
+
+    public static void readIn(BufferedReader in) throws IOException {
+        String string = in.readLine();
+        System.out.println(string);
     }
 
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,15 +8,34 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        int port = 8088;
+        int port = 7777;
         System.out.println("New connection accepted");
         ServerSocket serverSocket = new ServerSocket(port);
         Socket clientSocket = serverSocket.accept();
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+        printOut(out, "Hallo! Write your name, please!");
+
         final String name = in.readLine();
-        out.println(String.format("Hi, %s, your port is %d", name, clientSocket.getPort()));
+
+        printOut(out, "Are you child? (yes/no)");
+
+        final String answer = in.readLine();
+
+        if ("yes".equals(answer)) {
+            out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+            out.flush();
+        } else {
+            out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+            out.flush();
+        }
 
     }
+
+    public static void printOut(PrintWriter out, String string) {
+        out.println(string);
+        out.flush();
+    }
+
 }
